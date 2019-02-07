acbd947c120aa2dc5e2aceb67b6b7a9cfe024b89
hugoder <hugo.derri@edu.ece.fr>
2019-02-08 00:51:30 +0100

    liaison des fichiers et ajout d'une image

--- tp3_hugo_derri/src/App.js
+++ tp3_hugo_derri/src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import profil from './Component/profil.js';
+import jeanne from './Component/ppjeanne.jpg';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="Navigateur">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Mon navigateur
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <hr/>
+        <profil>
+        <img className="img"src={jeanne} />
+        
+        </profil>
       </div>
     );
   }
---
+++ tp3_hugo_derri/src/Component/profil.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './profil.css';
+
+class profil extends Component {
+      render() {
+      return (
+        <center>
+        <div className="profil" >
+            <img src="ppjeanne.jpg" className="img" />
+        </div>
+      </center>
+        );
+      }
+    }
